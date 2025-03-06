a30a028a83e124dd2a758c12ca9b858610773b27
ollatomiwa <ajaomoshood@gmail.com>
2025-03-06 22:24:20 +0100

    ROUTING FIX

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -61,7 +61,7 @@ const closeMobileMenu = () => {
             <NuxtLink @click="closeMobileMenu" to="/" class="flex items-center">Home</NuxtLink>
           </li>
           <li class="p-2 text-sm text-white font-bold">
-            <NuxtLink @click="closeMobileMenu" to="#about" class="flex items-center">About</NuxtLink>
+            <NuxtLink @click="closeMobileMenu" to="/about" class="flex items-center">About</NuxtLink>
           </li>
           <li class="p-2 text-sm text-white font-bold">
             <NuxtLink @click="closeMobileMenu" to="#contact" class="flex items-center">Contact</NuxtLink>
